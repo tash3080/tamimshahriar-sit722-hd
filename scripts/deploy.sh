45538555c366d2fc8687d3b553466b753380519d
Tamim Shahriar <tash3080@gmail.com>
2024-10-09 14:47:27 +1100

    workflow update 26

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,8 @@
 # Deploys the Node.js microservice to Kubernetes.
 #
 
-# set -u # or set -o nounset
-# : "$AZURE_RG"
-# : "$AZURE_AKS"
-
-# az aks get-credentials --resource-group $AZURE_RG --name $AZURE_AKS --overwrite-existing
+set -u # or set -o nounset
+: "$VERSION"
 
 envsubst < ./scripts/kubernetes/deployment.yaml | kubectl delete -f - 
 envsubst < ./scripts/kubernetes/deployment.yaml | kubectl apply -f - 
