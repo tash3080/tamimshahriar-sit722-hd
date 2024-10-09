c7c7bddacf42c71c42ad2b53035c79a8bfb51feb
Tamim Shahriar <tash3080@gmail.com>
2024-10-09 15:05:04 +1100

    workflow update 29

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,8 @@
 set -u # or set -o nounset
 : "$VERSION"
 : "$ECR_REGISTRY"
+: "$ECR_REPOSITORY1"
+: "$ECR_REPOSITORY2"
 
 envsubst < ./scripts/kubernetes/deployment.yaml | kubectl delete -f - 
 envsubst < ./scripts/kubernetes/deployment.yaml | kubectl apply -f - 
